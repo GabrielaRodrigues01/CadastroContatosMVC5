eb8bb247673c1bd098949e74f1998ac115d3ba80
Gabriela Rodrigues <78853618+GabrielaRodrigues01@users.noreply.github.com>
2026-04-09 22:12:09 -0300

    Inicio projeto

---
+++ CadastroContatosMVC5/CadastroContatosMVC5/Controllers/ContatoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CadastroContatosMVC5.Models;
+
+namespace CadastroContatosMVC5.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ContatoModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            TempData["MensagemSucesso"] = "Contato cadastrado com sucesso!";
+            return RedirectToAction("Sucesso");
+        }
+
+        public ActionResult Sucesso()
+        {
+            return View();
+        }
+    }
+}
---
+++ CadastroContatosMVC5/CadastroContatosMVC5/Models/ContatoModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastroContatosMVC5.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Digite um e-mail válido.")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "A cidade é obrigatória.")]
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [StringLength(300, ErrorMessage = "Máximo de 300 caracteres.")]
+        [Display(Name = "Observação")]
+        public string Observacao { get; set; }
+    }
+}
